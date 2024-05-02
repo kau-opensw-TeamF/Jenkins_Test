9cd6e7f69e0b9bfea7ebe78a5641fa67eafde14f
wooandrich <brad1182@naver.com>
2024-05-02 21:04:13 +0900

    first commit

---
+++ src/main/java/com/OSS/testJenkins/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.OSS.testJenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String Home() {
+        return "오픈소스입문 팀F Jenkins 첫번째 테스트입니다.";
+    }
+}
