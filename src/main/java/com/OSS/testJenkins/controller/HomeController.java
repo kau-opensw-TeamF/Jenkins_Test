48adc28dddb58d554f8377b26d0aff5f5d09af1c
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-05-03 17:13:34 +0900

    Update HomeController.java

--- src/main/java/com/OSS/testJenkins/controller/HomeController.java
+++ src/main/java/com/OSS/testJenkins/controller/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
     @RequestMapping("/")
     @ResponseBody
     public String Home() {
-        return "오픈소스입문 팀F Jenkins 첫번째 테스트입니다.";
+        return "오픈소스입문 팀F Jenkins 두번째 테스트입니다.";
     }
 }
